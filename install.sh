a5cfc05840c71a2c84413f48e9bd94b7d56a3577
anirudtate <anirud.programming@gmail.com>
2022-05-10 16:45:56 +0530

    .

---
+++ install.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+sudo pacman -S --noconfirm xorg-server sx xorg-xkill xorg-xsetroot xorg-xprop \
+     ttf-jetbrains-mono ttf-joypixels \
+     sxiv mpv zathura zathura-pdf-mupdf \
+     fzf man-db xwallpaper python-pywal unclutter xclip maim \
+     zip unzip unrar p7zip xdotool brightnessctl  \
+     dosfstools ntfs-3g git sxhkd zsh pipewire pipewire-pulse \
+     rsync dash \
+     xcompmgr thunar gvfs gvfs-mtp nnn \
+     pamixer zsh-autosuggesions zsh-completions zsh-history-substring-search \
+     zsh-syntax-highlighting xdg-user-dirs
+
+#part3
+printf '\033c'
+cd $HOME
+git clone --separate-git-dir=$HOME/.dotfiles git@gihhub.com:anirudtate/dotfiles.git tmpdotfiles
+rsync --recursive --verbose --exclude '.git' tmpdotfiles/ $HOME/
+rm -r tmpdotfiles
+# dwm: Window Manager
+git clone --depth=1 git@github.com:anirudtate/dwm.git ~/.local/src/dwm
+sudo make -C ~/.local/src/dwm install
+
+# st: Terminal
+git clone --depth=1 git@github.com:anirudtate/st.git ~/.local/src/st
+sudo make -C ~/.local/src/st install
+
+# dmenu: Program Menu
+git clone --depth=1 git@github.com:anirudtate/dmenu.git ~/.local/src/dmenu
+sudo make -C ~/.local/src/dmenu install
+
+# pikaur: AUR helper
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -fsri
+cd
+yay -S dashbinsh
+
+alias dot='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+dot config --local status.showUntrackedFiles no
+exit
